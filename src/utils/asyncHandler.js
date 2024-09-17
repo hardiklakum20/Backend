ff5376b74f0d52455b46c280a902b868e1aeb672
hardiklakum20 <120251398+hardiklakum20@users.noreply.github.com>
2024-09-17 21:56:13 +0530

    trying

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -9,8 +9,6 @@ export {asyncHandler}
 // const asyncHandler =() => {}
 // const asyncHandler = (func) => () => {}
 // const asyncHandler = (func) => async() => {}
-
-
 // const asyncHandler = (fn) => async(req,res,next) => {
 //     try {
 //         await fn(req,res,next)        
